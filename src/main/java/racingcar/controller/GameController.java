56100cb5b47557a363fa61f9d74bb04828c39800
stdiodh <playlistdh@gmail.com>
2025-10-23 11:35:43 +0900

    feat(input): 자동차 이름 및 시도 횟수 입력 기능 구현

--- src/main/java/racingcar/controller/GameController.java
+++ src/main/java/racingcar/controller/GameController.java
@@ -1,4 +1,25 @@
 package racingcar.controller;
 
+import racingcar.view.InputView;
+import racingcar.view.OutputView;
+
 public class GameController {
+    private final InputView inputView;
+    private final OutputView outputView;
+
+    public GameController() {
+        this.inputView = new InputView();
+        this.outputView = new OutputView();
+    }
+
+    public void run() {
+        setupGame();
+    }
+
+    private void setupGame() {
+        outputView.printCarNamePrompt();
+        String rawName = inputView.readInput();
+        outputView.printTryCountPrompt();
+        String rawCount = inputView.readInput();
+    }
 }
--- src/main/java/racingcar/view/OutputView.java
+++ src/main/java/racingcar/view/OutputView.java
@@ -1,4 +1,14 @@
 package racingcar.view;
 
 public class OutputView {
+    private static final String PROMPT_CAR_NAMES = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
+    private static final String PROMPT_TRY_COUNT = "시도할 횟수는 몇 회인가요?";
+
+    public void printCarNamePrompt() {
+        System.out.println(PROMPT_CAR_NAMES);
+    }
+
+    public void printTryCountPrompt() {
+        System.out.println(PROMPT_TRY_COUNT);
+    }
 }
