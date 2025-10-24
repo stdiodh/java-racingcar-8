266ec95814e7b59f7868843a67e99be066f97ab9
stdiodh <playlistdh@gmail.com>
2025-10-24 14:49:10 +0900

    refactor(view, controller): InputView 캡슐화 강화 및 중복 제거
    `InputView`가 '프롬프트 출력 + 콘솔 입력'을
    `readCarNames()`, `readTryCount()` 메서드 내에서 캡슐화하도록 수정함.
    
    이를 통해 Controller는 View의 세부 구현을 알 필요 없이 요청만 하도록
    책임을 축소하고, View와의 결합도를 낮춤.

--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -3,6 +3,19 @@ package racingcar.view;
 import camp.nextstep.edu.missionutils.Console;
 
 public class InputView {
+    private static final String PROMPT_CAR_NAMES = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
+    private static final String PROMPT_TRY_COUNT = "시도할 횟수는 몇 회인가요?";
+
+    public String readCarNames() {
+        System.out.println(PROMPT_CAR_NAMES);
+        return readInput();
+    }
+
+    public String readTryCount() {
+        System.out.println(PROMPT_TRY_COUNT);
+        return readInput();
+    }
+
     public String readInput() {
         return Console.readLine();
     }
